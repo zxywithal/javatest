0b1ab01ecc4e3dcec369a9c18a27dd773aa0c59e
zhangxiaoyun3 <zhangxiaoyun3@jd.com>
2021-06-28 09:55:37 +0800

    实现cookie打印的逻辑

--- testCors/src/testCors/CookieServlet.java
+++ testCors/src/testCors/CookieServlet.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@WebServlet
+@WebServlet(urlPatterns = "/cookie")
 public class CookieServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -28,14 +28,20 @@ public class CookieServlet extends HttpServlet {
             cookies = Arrays.stream(req.getCookies()).filter(obj->obj.getName().equals("name") || obj.getName().equals("age")).collect(Collectors.toList());
         }
 
-        if(cookies.isEmpty()){
+        if (cookies.isEmpty()) {
             Cookie cookie = new Cookie("name", "YourBatman");
             cookie.setMaxAge(3600);
+            cookie.setDomain("localhost");
             resp.addCookie(cookie);
             Cookie age = new Cookie("age", "18");
             age.setMaxAge(3600);
+            age.setDomain("localhost");
             resp.addCookie(age);
+        } else {
+            cookies.forEach(obj->{
+                System.out.println("name ["+obj.getName()+"] value ["+obj.getValue()+"] domain ["+obj.getDomain()+"] path ["+obj.getPath()+"] maxAge ["+obj.getMaxAge()+"] version ["+obj.getVersion()+"] source ["+obj.getSecure()+"]");
+            });
         }
-
+        resp.getWriter().write("hello cookie...");
     }
 }
